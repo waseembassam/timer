24f4fcf628a66cef4563169f3123850419aee541
waseembassam <waseembassamayesh@gmail.com>
2017-02-08 10:39:53 +0200

    Add files via upload

---
+++ test2.js
@@ -0,0 +1,7 @@
+QUnit.test("Function `program` should exist", function(t) {
+  t.ok(window.program,'it exists');
+});
+
+QUnit.test("function 'setInterval' starte after 15 min ", function(t)){
+  
+}
