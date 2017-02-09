9eb61f616557331c01b34240b1693d76d8841580
waseembassam <waseembassamayesh@gmail.com>
2017-02-09 12:56:22 +0200

    Update src2.js

--- src2.js
+++ src2.js
@@ -1,66 +1,39 @@
 
-  var timer_is_on = true;
+var timer_is_on = true;
 var sec=600,
   countDiv = document.getElementById('countdown'),
-      
-
   secondPass,
-
   countdown = setInterval(function(){
-    secondPass()
-
+  secondPass()
   } ,1000);
-  
   function secondPass() {
-    
     var min = Math.floor(sec/60);
-    remsec = sec% 60;
-
+  remsec = sec% 60;
     if (remsec < 10) {
-      remsec = "0" + remsec;
-
+  remsec = "0" + remsec;
     }
     if (min < 10) {
       min = "0" + min;
-
     }
-      
-
-    countDiv.innerHTML = min + ':' + remsec;
+  countDiv.innerHTML = min + ':' + remsec;
     if (sec > 0){
-
       sec = sec -1 ;
     }else{
       clearInterval(countdown);
-      countDiv.innerHTML = "Done";
-
-    }
-    
-}
-
+      countDiv.innerHTML = "Done";}}
 
 function yts(){
  sec = document.getElementById("field").value*60;
 }
-
 function reset (){
-    sec=document.getElementById("field").value*60;
+ sec=document.getElementById("field").value*60;
 }
  function startCount() {
     if (!timer_is_on) {
-        timer_is_on = true;
-       setInterval(function(){
+    timer_is_on = true;
+    setInterval(function(){
     secondPass()
-
-  } ,1000);
-    }
-
-}
-
+  } ,1000);}}
 function stopCount() {
     clearInterval(countdown);
-    timer_is_on = false;
-}
-
-
-
+    timer_is_on = false;}
